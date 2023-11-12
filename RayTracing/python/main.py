8b3c3cd2a5a56715136ad77336499795903d133b
JeniRag <sujeni@live.com>
2023-11-12 23:15:33 +0100

    matrixes

--- RayTracing/python/main.py
+++ RayTracing/python/main.py
@@ -24,7 +24,7 @@ if __name__=="__main__":
 
 
     O=np.array([0.0, 0.0, -1.0]) #origin (camera position)
-    Light_source=np.array([-0.1,0.6, 0.8])
+    Light_source=np.array([-1.0,0.0, 0.8])
     
     #make sure light source is not inside the object
     for obj in objects:
@@ -36,16 +36,35 @@ if __name__=="__main__":
     h=400
     w=400
 
-    pixel_object = Pixels(h,w)
-
+    # pixel_object = Pixels(h,w)
     
-    start = time.time()
-    pixel_traced = ray_casting(pixel_object,objects, O)
-    pixel_color=compute_illumination(Light_source,pixel_traced, objects)
-    end = time.time()
-    print(f"{np.round(end-start,2)} s")
+    # start = time.time()
+    # pixel_traced = ray_casting(pixel_object,objects, O)
+    # pixel_color=compute_illumination(Light_source,pixel_traced, objects)
+    # end = time.time()
+    # print(f"with loops: {np.round(end-start,2)} s")
 
-    #%%
+    # plt.figure()
+    # plt.title("shadow included")
+    # plt.imshow(pixel_color)
+    
+    pixel_object = Pixels(h,w)
+    start =  time.time()
+    pixel_traced = ray_casting_matrix(pixel_object, objects, O)
+    print("illumination--------------")
+    pixel_color = compute_illumination_matrix(Light_source, pixel_traced, objects)
+    end = time.time()
+    print(f"with matrix: {np.round(end-start,2)} s")
     plt.figure()
-    plt.title("shadow included")
     plt.imshow(pixel_color)
+    
+    # intermediate_color = np.zeros_like(pixel_object.iHit)
+    # for i in range(intermediate_color.shape[0]):
+    #     for j in range(intermediate_color.shape[1]):
+    #         idx = pixel_traced.iobj[i][j] 
+    #         if np.isnan(idx) == False:
+    #             idx = int(idx)
+    #             intermediate_color[i][j]=objects[idx].color
+    # plt.figure()        
+    # plt.imshow(intermediate_color)
+    
--- RayTracing/python/ray_tracing.py
+++ RayTracing/python/ray_tracing.py
@@ -28,6 +28,21 @@ class Pixels():
         return
     
 def compute_primary_ray(ix, iy, O):
+    """
+    
+
+    Parameters
+    ----------
+    ix : float.
+    iy : float.
+    O : np.array (shape 1x3), The camera coordinates
+
+    Returns
+    -------
+    np.array
+        returns the primary ray normalized to 1.
+
+    """
     #O np.array
     #primary ray: ray from origin (camera) through pixel
     
@@ -37,6 +52,22 @@ def compute_primary_ray(ix, iy, O):
     return d/np.linalg.norm(d) #normalized ray
 
 def compute_primary_ray_matrix(pixel_matrix,O):
+    """
+    
+
+    Parameters
+    ----------
+    pixel_matrix : TYPE
+        DESCRIPTION.
+    O : TYPE
+        The camera coordinates.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
     d = np.zeros_like(pixel_matrix)
     d[:,:,0] = pixel_matrix[:,:,0]-O[0] #x
     d[:,:,1] = pixel_matrix[:,:,1] - O[1]
@@ -47,6 +78,24 @@ def compute_primary_ray_matrix(pixel_matrix,O):
     
 
 def intersects_sphere(obj, prim_ray, O):
+    """
+    
+
+    Parameters
+    ----------
+    obj : Sphere Class
+        DESCRIPTION.
+    prim_ray : np.array (shape 1x3)
+        the primary rays.
+    O : np.array (shape 1x3)
+        camera coordinates.
+
+    Returns
+    -------
+    bool or tuple
+        DESCRIPTION.
+
+    """
     
     a = np.dot(prim_ray, prim_ray) 
     b = 2*np.dot((O - obj.center), prim_ray)
@@ -73,69 +122,61 @@ def intersects_sphere(obj, prim_ray, O):
     else:
         return False
     
-def intersects_sphere_matrix(obj, prim_ray, O):
-    #O_matrix = np.zeros_like(prim_ray_matrix)
-    
+def intersects_sphere_matrix(obj, prim_ray, O_matrix):
+    print(f" prim_ray: {np.any(np.isnan(prim_ray))==False}")
+    print(prim_ray)
     a = prim_ray[:,:,0] * prim_ray[:,:,0] + prim_ray[:,:,1] * prim_ray[:,:,1] + prim_ray[:,:,2] * prim_ray[:,:,2]  #np.dot(prim_ray, prim_ray) 
+    
+    
+    print(f" a: {np.any(np.isnan(a))==False}")
+    
     #2D array
-    OR_diff = O - obj.center
+    OR_diff = O_matrix - obj.center
     #1D
     
-    b = 2* (OR_diff[0] * prim_ray[:,:;0] + OR_diff[1] * prim_ray[:,:,1] + OR_diff[2]* prim_ray[:,:,2])
+    b = 2* (OR_diff[:,:, 0] * prim_ray[:,:,0] + OR_diff[:,:,1] * prim_ray[:,:,1] + OR_diff[:,:,2]* prim_ray[:,:,2])
     #2D array
     
     #2*np.dot((O - obj.center), prim_ray)
-    c_temp = (OR_diff[0] * OR_diff[0] + OR_diff[1] * OR_diff[1] + OR_diff[2] * OR_diff[2]) - obj.radius**2
-    c= np.zeros_like(a)
-    c[:,:] = c_temp 
-    
-    
-    #np.dot((O - obj.center),( O - obj.center)) - obj.radius**2
+    c = (OR_diff[:,:,0] * OR_diff[:,:,0] + OR_diff[:,:,1] * OR_diff[:,:,1] + OR_diff[:,:,2] * OR_diff[:,:,2]) - obj.radius**2
+   
     
     D=b**2-4*a*c
+    # print(D)
     
     cond = (D>=0) #elementes where condition is fulfilled
+    # print(f"any D >=0: {np.any(D>=0)}")
     
-    q=np.zeros_like(a)
-    t1 = np.zeros_like(a)
-    t2= np.zeros_like(a)
-    t=np.zeros_like(a)
+    q=np.full_like(a, np.nan)
+    t1 =np.full_like(a, np.nan)
+    t2= np.full_like(a, np.nan)
+    t=np.full_like(a, np.nan)
     
     q[cond] = -0.5 * (b[cond] + np.sign(b)[cond] * np.sqrt(D[cond]))
     t1[cond] = q[cond] / a[cond]
     t2[cond] = c[cond] / q[cond]
 
-    tcond = (t1 >0 and t2 >0 and D>=0)
-    t[tcond] = np.min((t1, t2), axis=0)
+    tcond = ((t1 >0) & (t2 >0) & (D>=0))
+    t[tcond] = np.min((t1[tcond], t2[tcond]), axis=0)
+    t_matrix = np.full_like(prim_ray, np.nan)
+    t_matrix[:,:,0] = t
+    t_matrix[:,:,1] = t
+    t_matrix[:,:,2] = t
     
-
-    pHit = np.zeros_like(prim_ray)
-        
-    #pHit = prim_ray * t + O
+    pHit = np.full_like(prim_ray, np.nan)
+    pHit[tcond, :] = prim_ray[tcond,:] + t_matrix[tcond,:] + O_matrix[tcond,:]
     
-    if D >=0: #
-    
-        q = -0.5 * (b + np.sign(b) * np.sqrt(D))
-        t1 = q/a
-        t2 = c/q
+    N = np.full_like(prim_ray, np.nan)
+    N[tcond, :] =( pHit[tcond, :] - obj.center) / obj.radius
+    #normalize?
    
-        if t1>0 and t2>0:
-            t=min(t1,t2)
-            
-        
-            p_Hit = prim_ray *t + O
-            
-            N = (p_Hit- obj.center) / obj.radius
-            # N /= np.linalg.norm(N) #normalize
-        
-            return (p_Hit, N)   
+    print(f"t cond {np.any(tcond == True)}")
+    return pHit, N, tcond
      
-    else:
-        return False
-    
     
     
 def ray_casting(pixel_object, objects, O):
+    
 
     #y: vertical, x: horizontal
 
@@ -168,27 +209,35 @@ def ray_casting(pixel_object, objects, O):
 def ray_casting_matrix(pixel_object, objects, O):
     #y: vertical, x: horizontal
 
-    prim_ray_matrix = compute_primary_ray_matrix(pixel_object.matrix, O)
-    
-    for i in range(pixel_object.height):
-        for j in range(pixel_object.width):
 
-            prim_ray = prim_ray_matrix[i,j,:]
-            min_dist = np.inf #initiate value for minimal distance
-            
-            for obj_idx, obj in enumerate(objects):
-                intersecting = intersects_sphere(obj, prim_ray, O)
-                if intersecting != False:
-                    p_Hit, N = intersecting
-                    dist = np.linalg.norm(O-p_Hit) # distance between camera point and hitting point
-                    
-                    
-                    if dist < min_dist :
-                        min_dist = dist #update minimal distance
-                        pixel_object.iobj[i][j] = obj_idx  #save which object is visible in the pixel
-                        pixel_object.iHit[i][j]= p_Hit
-                        pixel_object.iN[i][j] = N
-                            
+    prim_ray_matrix = compute_primary_ray_matrix(pixel_object.matrix, O)
+    min_dist = np.full_like(prim_ray_matrix[:,:,0], np.inf)
+    
+    O_matrix = np.full_like(prim_ray_matrix, np.nan)
+    O_matrix[:,:,0] = O[0]
+    O_matrix[:,:,1] = O[1]
+    O_matrix[:,:,2] = O[2]
+    
+    for obj_idx, obj in enumerate(objects):
+        pHit, N, is_intersecting = intersects_sphere_matrix(obj, prim_ray_matrix, O_matrix)
+        print(f"ray casting : {np.any(is_intersecting == True)}")
+        
+        # not_intersecting = np.isnan(pHit[:,:,0]) #condition same as _
+        # is_intersecting = ~not_intersecting
+        
+        dist = np.full_like(min_dist, np.inf)
+        
+        dist[is_intersecting] = np.linalg.norm((O_matrix - pHit), axis=2)[is_intersecting]
+        
+        is_smaller = dist < min_dist
+        
+        update = is_intersecting * is_smaller
+        
+        min_dist[update] = dist[update]
+        pixel_object.iobj[update] = obj_idx
+        pixel_object.iHit[update, :] = pHit[update]
+        pixel_object.iN[update, :] = N[update]
+                                 
     return pixel_object
 
 def compute_illumination(L, pixel_object, objects):
@@ -225,6 +274,62 @@ def compute_illumination(L, pixel_object, objects):
                    
                 
     return pixel_color
+
+
+def compute_illumination_matrix(L, pixel_object, objects):
+    # L light position
+    
+    L_matrix = np.zeros_like(pixel_object.iHit)
+    L_matrix[:,:,0] = L[0]
+    L_matrix[:,:,1] = L[1]
+    L_matrix[:,:,2] = L[2]
+    
+    pixel_color = np.zeros((pixel_object.height, pixel_object.width, 3), dtype = float)
+    
+    not_visible = np.isnan(pixel_object.iobj) #condition
+    is_visible = ~not_visible
+    print(f"any are visible {np.any(is_visible == True)}")
+    
+    shadow_direction = np.full_like(pixel_object.iHit, np.nan)
+    shadow_origin = np.full_like(pixel_object.iHit, np.nan)
+    
+    shadow_direction[is_visible,:] = L_matrix[is_visible,:] - pixel_object.iHit[is_visible,:] 
+   
+    norm = np.linalg.norm(shadow_direction, axis=2)[:,:, np.newaxis]
+    shadow_direction /=norm
+
+    print(f"shadow norm: {np.any(np.isnan(norm) == False)}")
+    print(f"shadow normalized: {np.any(np.isnan(shadow_direction) == False)}")
+    
+    shadow_origin[is_visible,:] = pixel_object.iHit[is_visible,:] + pixel_object.iN[is_visible,:] *1e-1
+    
+    is_shadow = np.full_like(pixel_object.iobj, False, dtype = bool)
+    
+    for other_obj in objects:
+        _, _, is_intersecting = intersects_sphere_matrix(other_obj, shadow_direction, shadow_origin)
+        # print(f"any not nans: {np.any(np.isnan(shadow_origin) == False)}")
+        print(f"illumination any intersecting: {np.any(is_intersecting)==True}")
+        
+        is_shadow[is_intersecting] = True
+        pixel_color[is_intersecting,:] = np.zeros(3)
+        # print(np.any(is_shadow == True))
+        #pixel color stays black
+        
+    not_shadow = ~is_shadow
+    print(f"There are any shadows: {np.any(is_shadow == True)}")
+    # find a way to avoid for loop
+    cond = not_shadow * is_visible
+    print(f"There are visible and not visible: {np.any(cond == False) * np.any(cond == True)}")
+    for i in range(pixel_object.height):
+        for j in range(pixel_object.width):
+            if cond[i][j]:
+                obj_idx = int(pixel_object.iobj[i][j])
+                pixel_color[i,j,:] = objects[obj_idx].color
+           
+                
+    return pixel_color
+
+
 
 def is_point_in_sphere(P, S, R):
     #point in sphere if R >= distance
